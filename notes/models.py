4bbf831a2dac06294d4b2f242d598645600092f2
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-08-01 00:53:19 -0400

    subject and sub-subject blank

--- notes/models.py
+++ notes/models.py
@@ -5,8 +5,15 @@ from django.conf import settings
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    subject = models.CharField(max_length=50)
-    sub_subject = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50, blank=True)
+    sub_subject = models.CharField(max_length=50, blank=True)
+    priority_choices = [
+        ('High', 'High'),
+        ('Normal', 'Normal'),
+        ('Low', 'Low'),
+    ]
+    priority = models.CharField(
+        max_length=10, choices=priority_choices, default='Normal')
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
