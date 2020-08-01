0ca6534b78e92b644f839b143c4444fc2eccb844
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-08-01 00:36:39 -0400

    created and migrated Note model

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
 
-
-# class Note(models.Model):
-#     title = models.CharField(max_length=200)
-#     body = models.TextField()
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    subject = models.CharField(max_length=50)
+    sub_subject = models.CharField(max_length=50)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
