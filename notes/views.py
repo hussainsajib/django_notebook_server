5ac076daf8759358edd3703924ca95d0bc7722da
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-08-16 16:19:33 -0400

    restricted notes routes for logged in users only

--- notes/views.py
+++ notes/views.py
@@ -1,20 +1,21 @@
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Note
 from .forms import NoteForm
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Note
     template_name = 'notes.html'
 
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Note
     template_name = 'note_detail.html'
 
 
-class NoteCreateView(CreateView):
+class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Note
     form_class = NoteForm
     template_name = 'new_note.html'
